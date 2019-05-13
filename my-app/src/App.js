9fff9d01c5f0d12dc8941a51d53ae0a2a52b55d7
gellyviana <gellyviana@outlook.com>
2019-05-13 19:12:12 -0300

    Adicinado Form com dados atualizados automaticamente na tabela

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,10 @@
 import React, {Component} from 'react'
 import Table from './Table';
+import Form from './Form';
 
 class App extends Component{
     state = {
-        characters:[
-            {
-                name: 'Charlie',
-                job: 'Janitor',
-            },
-            {
-                name:'Mac',
-                job:'Bouncer'
-            },
-            {
-                name:'Dee',
-                job:'Aspiring actress',
-            },
-            {
-                name:'Dennis',
-                job:'Bartender',
-            },
-        ]
+        characters:[]
     };
     removeCharacter = index => {
         const { characters } = this.state;
@@ -31,11 +15,17 @@ class App extends Component{
             })
         });
     }
+    handleSubmit = character => {
+        this.setState({characters:[...this.state.characters,character]})
+    }
     render(){
         
         return (
             <div className = "container">
+            <h1>React Tutorial</h1>
+            <p>Add a character with a name and job to the table</p>
                 <Table characterData={this.state.characters} removeCharacter={this.removeCharacter} />
+                <Form handleSubmit={this.handleSubmit}/>
             </div>
         )
     }
